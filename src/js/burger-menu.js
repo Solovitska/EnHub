39405b5717993f8e6adedc0933fdb0c79099d931
unknown <tanish@ua.i>
2024-09-02 16:13:05 +0300

    js

---
+++ src/js/burger-menu.js
@@ -0,0 +1,20 @@
+// Функція для перемикання активного класу
+function toggleNav() {
+  const nav = document.querySelector('.about-us-nav');
+  const burgerMenu = document.querySelector('.burger-menu');
+
+  if (nav && burgerMenu) {
+    // Додаємо або прибираємо активний клас
+    nav.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+  }
+}
+
+// Додаємо слухач подій до меню-бургера
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerMenuContainer = document.querySelector('.burger-menu-container');
+
+  if (burgerMenuContainer) {
+    burgerMenuContainer.addEventListener('click', toggleNav);
+  }
+});
