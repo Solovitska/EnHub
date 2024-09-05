4d41296bad524983e9ebb7c2bcad254ba2b3eac4
unknown <tanish@ua.i>
2024-09-05 14:37:43 +0300

    css

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,26 +1,10 @@
-function toggleNav() {
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.querySelector('.burger');
   const nav = document.querySelector('.about-us-nav');
-  const burgerMenu = document.querySelector('.burger-menu');
 
-  console.log('Nav:', nav);
-  console.log('Burger Menu:', burgerMenu);
-
-  if (nav && burgerMenu) {
-    nav.classList.toggle('active');
-    burgerMenu.classList.toggle('active');
-  } else {
-    console.error('Navigation or burger menu elements not found');
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerMenuContainer = document.querySelector('.burger-menu-container');
-
-  console.log('Burger Menu Container:', burgerMenuContainer);
-
-  if (burgerMenuContainer) {
-    burgerMenuContainer.addEventListener('click', toggleNav);
-  } else {
-    console.error('Burger menu container not found');
-  }
+  burger.addEventListener('click', function () {
+    nav.classList.toggle('open');
+    burger.classList.toggle('active');
+  });
 });
